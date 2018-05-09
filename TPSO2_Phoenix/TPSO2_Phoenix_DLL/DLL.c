4dc5832e105d2c56f5faec65fbb55d037c591fc4
alexprotas <alex_xanu42@hotmail.com>
2018-05-09 02:56:38 +0100

    DLL e piças

--- TPSO2_Phoenix/TPSO2_Phoenix_DLL/DLL.c
+++ TPSO2_Phoenix/TPSO2_Phoenix_DLL/DLL.c
@@ -1,9 +1,9 @@
 // Trata funcoes que lidem com o Buffer
-// Vai ser lida e escrita pelo Gateway
+// Vai ser lida e escrita pelo Gateway -> thread que lide com msgs recebidas do gateway -> thread que a envie para o server?
 	// Vai receber MSGs de Clientes (escrita pelo Gateway)
 		// dados de login?
 		// 
-// Vai ser lida e escrita pelo Server
+// Vai ser lida e escrita pelo Server -> thread que lide com msgs recebidas do server -> thread que as envie para o(s) cliente(s)?
 
 #include <windows.h>
 #include "DLL.h"
--- TPSO2_Phoenix/TPSO2_Phoenix_DLL/DLL.h
+++ TPSO2_Phoenix/TPSO2_Phoenix_DLL/DLL.h
@@ -1,16 +1,15 @@
 #include <windows.h>
 #include <tchar.h>
 
-#ifdef DLL_EXPORTS							// modo standard de criar macros que tornam a exportação funções e variáveis mais simples.
-#define DLL_IMP_API _declspec(dllexport)	// Todos os ficheiros neste projeto DLL compilados com o símbolo DLL_IMP_EXPORTS definido
+#ifdef DLL_EXPORTS
+#define TPSO2_Phoenix_DLL_IMP_API __declspec(dllexport)
 #else
-#define DLL_IMP_API _declspec(dllimport)
+#define TPSO2_Phoenix_DLL_IMP_API __declspec(dllimport)
 #endif
-
-extern C
+extern "C"
 {
 	//Variável global da DLL
-	extern DLL_IMP_API int nDLL;
+	extern TPSO2_Phoenix_DLL_IMP_API int nDLL;
 
 	//Funções a serem exportadas/importadas
 }
