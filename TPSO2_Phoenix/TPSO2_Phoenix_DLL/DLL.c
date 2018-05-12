6a6cf433dfff7e0450b4f5888f358f2b29d5e35a
alexprotas <alex_xanu42@hotmail.com>
2018-05-12 03:12:37 +0100

    é que nem desconfio se é mesmo isto mas whatever tentei, shit pa isto

--- TPSO2_Phoenix/TPSO2_Phoenix_DLL/DLL.c
+++ TPSO2_Phoenix/TPSO2_Phoenix_DLL/DLL.c
@@ -4,6 +4,18 @@
 		// dados de login?
 		// 
 // Vai ser lida e escrita pelo Server -> thread que lide com msgs recebidas do server -> thread que as envie para o(s) cliente(s)?
+/*
+6 FUNCOES
+
+	3 ZONAS DE MEMORIA PARTILHADA
+
+	COMUNICACAO GATEWAY - SERVIDOR
+		FUNCOES READ E WRITE
+	COMUNICACAO SERVIDOR - GATEWAY
+		FUNCOES READ E WRITE
+	REPRESENTACAO DO CAMPO PARA O GATEWAY LER
+		FUNCOES READ E WRITE
+*/
 
 #include <windows.h>
 #include "DLL.h"
@@ -15,3 +27,81 @@ char ponteiro[40960];
 int nDLL = 1234;
 
 //Exportar funcoes para ser utilizadas fora da DLL
+
+// COMUNICACAO GATEWAY -> SERVIDOR
+// FUNCAO READ
+// le uma msg
+// copia o conteudo da msg da zona de memoria partilhada para o processo 
+void readMSGGateway(ContrData * pcdata, Shared_MSG * msg) {
+	WaitForSingleObject(pcdata->hRWMutex, INFINITE);
+	// 1x CopyMemory - toda a info esta no mesmo sitio
+	CopyMemory(msg, pcdata->shared, sizeof(Shared_MSG));
+	ReleaseMutex(pcdata->hRWMutex);
+}
+
+// FUNCAO WRITE
+// escreve uma msg
+// copia o conteudo da zona nao partilhada do processo para a memoria partilhada
+void writeMSGGateway(ContrData * pcdata, TCHAR * msgtext) {
+	WaitForSingleObject(pcdata->hRWMutex, INFINITE);
+	pcdata->shared->msgnum++;
+	// se nao houver \0 as duas linhas seguintes falham
+	// (por isso é que dá, e muito bem, warnings)
+	_tcscpy(pcdata->shared->szOP, pcdata->MyName);
+	_tcscpy(pcdata->shared->szMessage, msgtext);
+	// 2x tcscpy porque a info vem de 2 sitios diferentes (szOP e szMessage)
+	ReleaseMutex(pcdata->hRWMutex);
+}
+
+// COMUNICACAO SERVIDOR -> GATEWAY
+// FUNCAO READ
+// le uma msg
+// copia o conteudo da msg da zona de memoria partilhada para o processo 
+void readMSGServer(ContrData * pcdata, Shared_MSG * msg) {
+	WaitForSingleObject(pcdata->hRWMutex, INFINITE);
+	// 1x CopyMemory - toda a info esta no mesmo sitio
+	CopyMemory(msg, pcdata->shared, sizeof(Shared_MSG));
+	ReleaseMutex(pcdata->hRWMutex);
+}
+
+// FUNCAO WRITE
+// escreve uma msg
+// copia o conteudo da zona nao partilhada do processo para a memoria partilhada
+void writeMSGServer(ContrData * pcdata, TCHAR * msgtext) {
+	WaitForSingleObject(pcdata->hRWMutex, INFINITE);
+	pcdata->shared->msgnum++;
+	// se nao houver \0 as duas linhas seguintes falham
+	// (por isso é que dá, e muito bem, warnings)
+	_tcscpy(pcdata->shared->szOP, pcdata->MyName);
+	_tcscpy(pcdata->shared->szMessage, msgtext);
+	// 2x tcscpy porque a info vem de 2 sitios diferentes (szOP e szMessage)
+	ReleaseMutex(pcdata->hRWMutex);
+}
+
+// sincronizar msgs recebidas na memoria partilhada
+// mecanismo de polling na detecao de msgs novas
+unsigned peekMSG(ContrData * pcdata) {
+	unsigned msgnum;
+	WaitForSingleObject(pcdata->hRWMutex, INFINITE);
+	msgnum = pcdata->shared->msgnum;
+	ReleaseMutex(pcdata->hRWMutex);
+	return msgnum;
+}
+
+BOOL WINAPI DllMain(HANDLE hInstance, DWORD razao, LPVOID tipo_declaracao) {
+	switch (razao) {
+	case DLL_PROCESS_ATTACH:
+		if (tipo_declaracao == NULL)
+			MessageBox(NULL, TEXT("O processou carregou a biblioteca explicitamente"), TEXT("DLLmain"), MB_OK);
+		else
+			MessageBox(NULL, TEXT("O processou carregou a biblioteca implicitamente"), TEXT("DLLmain"), MB_OK);
+		break;
+	case DLL_PROCESS_DETACH:
+		if (tipo_declaracao == NULL)
+			MessageBox(NULL, TEXT("O processou desmapeou a biblioteca explicitamente"), TEXT("DLLmain"), MB_OK);
+		else
+			MessageBox(NULL, TEXT("O processou desmapeou a biblioteca implicitamente"), TEXT("DLLmain"), MB_OK);
+		break;
+	}
+	return 1;
+}
--- TPSO2_Phoenix/TPSO2_Phoenix_DLL/DLL.h
+++ TPSO2_Phoenix/TPSO2_Phoenix_DLL/DLL.h
@@ -1,15 +1,50 @@
 #include <windows.h>
 #include <tchar.h>
+#include <string>
 
-#ifdef DLL_EXPORTS
+#define OPNAME_SZ 30
+#define MSGTEXT_SZ 75
+#define MSGBUFSIZE sizeof(Shared_MSG)
+#define MUTEX_NAME TEXT("RWMUTEX")
+
+TCHAR szName[] = TEXT("fmMSGSpace");
+
+// estrutura de dados para conter uma msg
+typedef struct _MSG {
+	unsigned msgnum;
+	TCHAR szOP[OPNAME_SZ];
+	TCHAR szMessage[MSGTEXT_SZ];
+}Shared_MSG;
+
+// a zona de memoria partilhada ira conter uma unica msg
+// existira uma unica vista dessa zona, que coincidira com a totalidade da memoria partilhada
+typedef struct _ControlData {
+	HANDLE hMapFile;
+	Shared_MSG * shared;
+	int ThreadDeveContinuar;
+	TCHAR MyName[OPNAME_SZ];
+	HANDLE hRWMutex;
+}ContrData;
+
+
+
+#ifdef TPSO2_Phoenix_EXPORTS
 #define TPSO2_Phoenix_DLL_IMP_API __declspec(dllexport)
 #else
 #define TPSO2_Phoenix_DLL_IMP_API __declspec(dllimport)
 #endif
+
+
+
 extern "C"
 {
 	//Variável global da DLL
 	extern TPSO2_Phoenix_DLL_IMP_API int nDLL;
 
 	//Funções a serem exportadas/importadas
+	TPSO2_Phoenix_DLL_IMP_API void readMSGGateway(ContrData * pcdata, Shared_MSG * msg);
+	TPSO2_Phoenix_DLL_IMP_API void writeMSGGateway(ContrData * pcdata, TCHAR * msgtext);
+	TPSO2_Phoenix_DLL_IMP_API void readMSGServer(ContrData * pcdata, Shared_MSG * msg);
+	TPSO2_Phoenix_DLL_IMP_API void writeMSGServer(ContrData * pcdata, TCHAR * msgtext);
+	TPSO2_Phoenix_DLL_IMP_API unsigned peekMSG(ContrData * pcdata);
 }
